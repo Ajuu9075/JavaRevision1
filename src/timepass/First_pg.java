bcc0687b1c91eaa2ec6ddec2c90856df16469ea7
Omkar C <chougule.omkar7@gmail.com>
2024-08-22 02:38:06 +0530

    (Ajinkya): 1st changes in project

---
+++ src/timepass/First_pg.java
@@ -0,0 +1,9 @@
+package timepass;
+
+public class First_pg {
+
+	public static void m3() {
+		
+		System.out.println("Static method m3 from another class");
+	}
+}
---
+++ src/timepass/First_program.java
@@ -0,0 +1,18 @@
+package timepass;
+
+public class First_program {
+	
+	
+	public static void main(String[] args) {
+		
+		 int i=1;
+		 
+		do
+		{
+			System.out.println(i);
+			i++;
+		}while(i<=5);
+	}
+
+	
+}
